d163ad7499454fa6708c7767220822d772b8f0e9
damian-ionut <143095557+damian-ionut@users.noreply.github.com>
2024-05-26 12:33:49 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+import { fetchContacts } from '../redux/contactOperations';
+import { selectError, selectIsLoading } from '../redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1 className={css.phonebookTitle}> Phonebook</h1>
+      <ContactForm />
+      <h2 className={css.contactsTitle}>Contacts</h2>
+      <Filter />
+      {isLoading && !error && (
+        <div className={css.loader}></div>
+      )}
+      <ContactList />
     </div>
   );
 };
